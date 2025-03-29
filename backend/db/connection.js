2a2f85ca7a36e1e1a118267fee606810fdee5bc6
Gitax18 <lusifer0418@gmail.com>
2025-03-30 01:57:25 +0530

    resolve DB connection bug

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose");
-const DB_Name = require("../constants.js")
+const DB_Name = require("../constants.js");
 
- const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}${DB_Name}`);
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MongoDB connection FAILED", error);
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_URI}`,
+      {
+        dbName: DB_Name,
+      }
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB connection FAILED", error);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
